bc96d5135550f716bf7dfd36e5bb43b43071a619
tenoritama <tenoritama.0623@gmail.com>
2015-02-27 00:53:54 +0900

    fix

--- tts/downloader.py
+++ tts/downloader.py
@@ -12,27 +12,6 @@ class TtsDownloader():
     def __init__(self, text=''):
         self.params = self.split_string(text)
 
-    def print_response(self):
-
-        f = open('/Users/tenoritama/test.mp3', 'w')
-        url = 'http://translate.google.com/translate_tts?tl=en&q=Hello.+Everybody.+How+are+you?+I+am+fine.'
-        #headers = { 'User-Agent' : user_agent, 'Content-Type': 'audio/mp3'}
-        headers = {"Host": "translate.google.com",
-                   "Referer": "http://www.gstatic.com/translate/sound_player2.swf",
-                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) "
-                                 "AppleWebKit/535.19 (KHTML, like Gecko) "
-                                 "Chrome/18.0.1025.163 Safari/535.19"
-        }
-
-        req = urllib2.Request(url,'', headers=headers)
-        res = urllib2.urlopen(req)
-        f.write(res.read())
-        # print response.read()
-        time.sleep(.5)
-        f.close()
-        #res = eval(response)['hypotheses']
-        #urllib.urlretrieve(url, '/Users/tenoritama/test.mp3')
-
     def split_string(self, text):
         tmp = re.sub(r'\s+', ' ', text)
         tmp = tmp.split(" ")
