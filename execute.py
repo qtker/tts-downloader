c32815aa78f0e557a12c321c1ed585ba1bd364d7
stenoritama <shr.nrht.0623@gmail.com>
2015-02-27 14:38:56 +0900

    fix

--- execute.py
+++ execute.py
@@ -3,5 +3,11 @@
 
 from tts.downloader import TtsDownloader
 
+# 文字列渡す場合
 tts = TtsDownloader('hello world')
-tts.download(path='/Users/hogehoge/test.mp3')
+tts.download(path='/Users/a13659/test.mp3')
+
+# url渡す場合
+url = 'http://translate.google.com/translate_tts?tl=en&q=hello+world'
+tts2 = TtsDownloader()
+tts2.download(url, '/Users/a13659/test2.mp3')
